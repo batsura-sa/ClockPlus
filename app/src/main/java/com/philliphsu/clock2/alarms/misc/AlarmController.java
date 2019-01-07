28939a69c36e85c231b963178a41d00b3ba91263
Sergey Batsura <batsura.sa@gmail.com>
2019-01-07 16:16:11 +0300

    add implementation 'com.android.support:design:25.1.1' for idea

--- app/src/main/java/com/philliphsu/clock2/alarms/misc/AlarmController.java
+++ app/src/main/java/com/philliphsu/clock2/alarms/misc/AlarmController.java
@@ -24,7 +24,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.*;
 import android.util.Log;
 import android.view.View;
 
